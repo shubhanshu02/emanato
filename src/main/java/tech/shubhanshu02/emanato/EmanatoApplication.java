ca64f9cc0441c7debe34b538b423ddcca994c64f
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-10-16 01:22:14 +0530

    Init Spring Project
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

---
+++ src/main/java/tech/shubhanshu02/emanato/EmanatoApplication.java
@@ -0,0 +1,13 @@
+package tech.shubhanshu02.emanato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmanatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmanatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/shubhanshu02/emanato/EmanatoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.shubhanshu02.emanato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmanatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
