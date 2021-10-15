99fd0d27970874b3b48d33d3ce6e8114885c097e
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-10-16 02:22:53 +0530

    Switch to Apache Tomcat Server
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

--- src/main/java/tech/shubhanshu02/emanato/EmanatoApplication.java
+++ src/main/java/tech/shubhanshu02/emanato/EmanatoApplication.java
@@ -2,12 +2,13 @@ package tech.shubhanshu02.emanato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmanatoApplication {
+public class EmanatoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmanatoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmanatoApplication.class, args);
+    }
 
 }
