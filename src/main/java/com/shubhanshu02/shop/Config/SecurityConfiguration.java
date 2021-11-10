1edd3be3e4108e51c51d24ca67a8fd8397fb4e2a
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-11-10 16:25:59 +0530

    Contact and 404 Page
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

--- src/main/java/com/shubhanshu02/shop/Config/SecurityConfiguration.java
+++ src/main/java/com/shubhanshu02/shop/Config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
